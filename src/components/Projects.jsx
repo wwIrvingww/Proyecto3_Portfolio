d69a3301e5c466c8b3499141bfde55a946153fec
wwIrvingww <mor22781@uvg.edu.gt>
2024-05-22 01:02:16 -0600

    particles added to projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,20 +3,30 @@ import React, { useState, useEffect } from 'react';
 import { useSpring, animated } from '@react-spring/web';
 import styles from '../styles/projects.module.css';
 import Carousel from './Carousel';
+import Particules from './Particules';
 import StaticCarousel from './Carousel';
 
 const Projects = () => {
   return (
-    <div className={styles.container}>
+    <>
+   
+   
+    <div className={styles.container}>   
+  
       <div className={styles.text}>
         <h1 className={styles.title}>My Projects</h1>
         <p className={styles.description}>
           These are some of my favorite projects. I&apos;m always looking for fun
           challenges to learn from. Any feedback is also appreciated!
         </p>
-      </div>
+      </div>  
       <Carousel />
+      <div className={styles.particleCont}>
+    <Particules />
     </div>
+    </div>
+ 
+    </>
     
   );
 }
