3ac3ec2d2a1cf38d58e67edef69a4f3f4952caa0
wwIrvingww <mor22781@uvg.edu.gt>
2024-05-22 12:24:30 -0600

    github image changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,9 +19,9 @@ export default function RootLayout({ children }) {
         <AnimatePresence mode="wait">
           <motion.div
             key={pathname}
-            initial={{ opacity: 0, x: 100 }}
+            initial={{ opacity: 0, x: -100 }}
             animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: -100 }}
+            exit={{ opacity: 0, x: 100 }}
             transition={{ duration: 0.5 }}
           >
             {children}
